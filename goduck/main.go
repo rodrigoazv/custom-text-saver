295b1d12e411115ee5a960f483d5ae8dd3715378
Rodrigo Azevedo <rodrigozcma@gmail.com>
2022-01-27 20:29:52 -0300

    removing unused kong_serve

--- goduck/main.go
+++ goduck/main.go
@@ -14,6 +14,6 @@ func main() {
 	}
 
 	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Println("Listing for requests at http://localhost:1333/hello")
+	log.Fatal(http.ListenAndServe(":1333", nil))
 }
