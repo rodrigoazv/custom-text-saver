2720a7634050a8b78a3b3c4611a0ca7eaa30fac1
Rodrigo Azevedo <rodrigozcma@gmail.com>
2022-01-27 20:23:04 -0300

    feat: adding freebird service in container and kong structure

--- free_bird/config/dev.exs
+++ free_bird/config/dev.exs
@@ -4,8 +4,8 @@ import Config
 config :free_bird, FreeBird.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
-  database: "free_bird_dev",
+  hostname: "postgres",
+  database: "postgres",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
@@ -18,7 +18,7 @@ config :free_bird, FreeBird.Repo,
 config :free_bird, FreeBirdWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
