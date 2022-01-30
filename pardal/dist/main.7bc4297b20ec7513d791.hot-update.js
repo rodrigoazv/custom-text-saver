35b7cada1d8d0f0002129d31083cc41f93a56b92
Rodrigo Azevedo <rodrigozcma@gmail.com>
2022-01-30 18:07:23 -0300

    partial

---
+++ pardal/dist/main.7bc4297b20ec7513d791.hot-update.js
@@ -0,0 +1,28 @@
+"use strict";
+self["webpackHotUpdatepardal"]("main",{
+
+/***/ "./src/common/main/http/api-url.ts":
+/*!*****************************************!*\
+  !*** ./src/common/main/http/api-url.ts ***!
+  \*****************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "makeApiUrl": function() { return /* binding */ makeApiUrl; }
+/* harmony export */ });
+var makeApiUrl = function (path) { return "http://localhost:3000".concat(path); };
+
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ !function() {
+/******/ 	__webpack_require__.h = function() { return "dbca997cfd0872667b01"; }
+/******/ }();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi43YmM0Mjk3YjIwZWM3NTEzZDc5MS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFDTyxJQUFNLFVBQVUsR0FBRyxVQUFDLElBQVksSUFBYSxzQ0FBd0IsSUFBSSxDQUFFLEVBQTlCLENBQThCOzs7Ozs7Ozs7VUNEbEYscUNBQXFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcGFyZGFsLy4vc3JjL2NvbW1vbi9tYWluL2h0dHAvYXBpLXVybC50cyIsIndlYnBhY2s6Ly9wYXJkYWwvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIlxuZXhwb3J0IGNvbnN0IG1ha2VBcGlVcmwgPSAocGF0aDogc3RyaW5nKTogc3RyaW5nID0+IGBodHRwOi8vbG9jYWxob3N0OjMwMDAke3BhdGh9YCIsIl9fd2VicGFja19yZXF1aXJlX18uaCA9IGZ1bmN0aW9uKCkgeyByZXR1cm4gXCJkYmNhOTk3Y2ZkMDg3MjY2N2IwMVwiOyB9Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
