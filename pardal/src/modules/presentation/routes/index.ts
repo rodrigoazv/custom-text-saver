738bf938b688f3cb2cb92b5bab51b15858df9420
Rodrigo Azevedo <rodrigozcma@gmail.com>
2022-01-30 18:06:16 -0300

    feat: adding scribe usecase to post article

--- pardal/src/modules/presentation/routes/index.ts
+++
@@ -1,9 +0,0 @@
-// main
-import { makeRoute } from '@/common/main/factories/routes';
-
-// routes
-import { Public } from './dash';
-
-export const Routes = makeRoute({
-  subroutes: [Public],
-});
--- pardal/src/modules/scribe/presentation/scribe-page/presentation.tsx
+++ pardal/src/modules/scribe/presentation/scribe-page/presentation.tsx
@@ -4,9 +4,10 @@ import { Container } from './styles'
 
 function ScribePresentation(props: any) {
     const { children } = props;
-  
+   
     return (
       <Container id="editorjs">
+        <button onClick={()=>props.saveData()}>Save</button>
       </Container>
     );
 };
