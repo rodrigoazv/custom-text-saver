1290ead44e64169494b0c23d3d4d414a3cadb160
Rodrigo Azevedo <rodrigozcma@gmail.com>
2022-02-06 13:18:34 -0300

    feat: adding worker to save and process image

--- goduck/main.go
+++ goduck/main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
-	"net/http"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/google/uuid"
 )
 
 func main() {
-	// Hello world, the web server
+	app := fiber.New()
+	app.Use(cors.New())
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	app.Static("/images", "./images")
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Wait a image\n")
+	// handle image uploading using post request
+
+	app.Post("/", handleFileupload)
+	app.Listen(":1333")
+
+}
+
+func handleFileupload(c *fiber.Ctx) error {
+
+	file, err := c.FormFile("image")
+
+	if err != nil {
+		log.Println("image upload error --> ", err)
+		return c.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
+
 	}
 
-	http.HandleFunc("/", helloHandler)
-	log.Println("Listing for requests at http://localhost:1333/")
-	log.Fatal(http.ListenAndServe(":1333", nil))
+	uniqueId := uuid.New()
+
+	filename := strings.Replace(uniqueId.String(), "-", "", -1)
+
+	fileExt := strings.Split(file.Filename, ".")[1]
+	image := fmt.Sprintf("%s.%s", filename, fileExt)
+
+	err = c.SaveFile(file, fmt.Sprintf("./images/%s", image))
+
+	if err != nil {
+		log.Println("image save error --> ", err)
+		return c.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
+	}
+
+	imageUrl := fmt.Sprintf("http://localhost:4000/images/%s", image)
+
+	data := map[string]interface{}{
+
+		"imageName": image,
+		"imageUrl":  imageUrl,
+		"header":    file.Header,
+		"size":      file.Size,
+	}
+
+	return c.JSON(fiber.Map{"status": 201, "message": "Image uploaded successfully", "data": data})
 }
